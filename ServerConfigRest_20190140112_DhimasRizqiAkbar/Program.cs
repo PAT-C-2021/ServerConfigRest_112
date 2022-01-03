2f8586da0d7b7383c205895618d4ee073fd27625
Dhimas <dhimas.ra48@gmail.com>
2022-01-03 22:06:10 +0700

    Add project files.

---
+++ ServerConfigRest_20190140112_DhimasRizqiAkbar/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using ServiceRest_20190140112_DhimasRizqiAkbar;
+
+namespace ServerConfigRest_20190140112_DhimasRizqiAkbar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObjek = null;
+            try
+            {
+                hostObjek = new ServiceHost(typeof(TI_UMY));
+                hostObjek.Open();
+                Console.WriteLine("SERVER sudahh READYYY");
+                Console.ReadLine();
+                hostObjek.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
